68ecb6ec87cf673255d86ffadf4f3c945153ed21
Iulian Sandu <none@none.com>
2025-08-08 19:02:42 +0300

    push the code build function

--- python/cloud_run_scraper_latest.py
+++ python/cloud_run_scraper_latest.py
@@ -34,14 +34,10 @@ def upload_blob(bucket_name, source_file_name, destination_blob_name):
     storage_client = storage.Client()
     bucket = storage_client.bucket(bucket_name)
     blob = bucket.blob(destination_blob_name)
-    
     generation_match_precondition = 0
-
     blob.upload_from_filename(source_file_name, if_generation_match=generation_match_precondition)
 
-    print(
-        f"File {source_file_name} uploaded to {destination_blob_name}."
-    )
+    print(f"File {source_file_name} uploaded to {destination_blob_name}.")
 
 @functions_framework.cloud_event
 def hello_pubsub(cloud_event):
